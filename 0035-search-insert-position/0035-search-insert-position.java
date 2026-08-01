fff5f28e3bc8fe33074ed050bb0e370709ae5bfa
Ipshita Jha <jhaipshita.mail@gmail.com>
2026-08-01 23:41:18 +0530

    Time: 0 ms (100%), Space: 44.7 MB (48.95%) - LeetHub

---
+++ 0035-search-insert-position/0035-search-insert-position.java
@@ -0,0 +1,24 @@
+class Solution {
+    public int searchInsert(int[] nums, int target) {
+        
+            int high=nums.length-1;
+            int low=0;
+            while(low<=high)
+            {
+           int mid=low+(high-low)/2;
+            if(target==nums[mid])
+            {
+                return mid;
+            }
+            else if(target<nums[mid])
+            {
+             high=mid-1;
+            }
+            else
+            {
+                low=mid+1;
+            }
+            }
+        return low;
+    }
+}
